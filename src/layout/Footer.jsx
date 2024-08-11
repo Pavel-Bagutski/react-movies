5a4956eb5e25e08238adbadc10f4b9d780540c05
gifte <pavelbahustki@gmail.com>
2024-08-11 21:39:49 +0300

    gh pages added

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+function Footer() {
+    return  <footer className="#4fc3f7 light-blue lighten-2" >
+   
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+} 
+export {Footer}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="#4caf50 green">
+    <div className="nav">
+      <a href="#" className="brand-logo">React Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Repo</a></li>
+      </ul>
+    </div>
+  </nav>
+}
+
+export {Header}
